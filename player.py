2137115d724be0602196447daef1790293513cd4
EduardoVeneza <eduardoveneza9@gmail.com>
2025-06-14 23:08:25 -0300

    [Server] Update in threads, player interface

--- player.py
+++ player.py
@@ -1,3 +1,14 @@
+import socket
+
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, conn: socket.socket, addr):
+        self.choice: int
+        self.name: str
+        self.addr = addr
+        self.conn = conn
+
+    def send_to_player(self, message) -> None:
+        self.conn.send(message.encode())
+
+    def receive_from_player(self):
+        return self.conn.recv(1024).decode()
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+from player import Player
 from enum import Enum
 import sys
 
@@ -13,15 +14,15 @@ class Game:
     def __init__(self):
         pass
     
-    def show_menu(self) -> None: 
-        title = " ROCK, PAPER AND SCISSORS "
-        print(title.center(54, "="))
-        menu = f'''Please, enter the respective number:
+    def get_menu(self) -> str: 
+        menu = f'''
+        /-/-/-/-/-/ ROCK PAPER SCISSOR /-/-/-/-/-/
+        Please, enter the respective number:
         1 - ROCK
         2 - PAPER
         3 - SCISSORS
         [Another Number] - EXIT GAME'''
-        print(menu)
+        return menu
 
     def verify_choice(choice: int) -> None:
         if choice < 1 or choice > 3:
@@ -33,10 +34,11 @@ class Game:
 # Holds the game server
 class Game_Server:
     def __init__(self, host = 'localhost', port = 5555) -> None:
+        self.game = Game()
         self.host = host
         self.port = port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clients = []  # Lista de clientes conectados
+        self.players: list[Player] = []  # Lista de jogadores conectados
 
     def start_server(self) -> None:
         self.server.bind((self.host, self.port))    
@@ -44,23 +46,47 @@ class Game_Server:
         print(f'Servidor Iniciado em ({self.host}, {self.port})')
 
         while True:
+            player = Player() # Jogador temporário para receber o socket OBJ e ip
             conn, addr = self.server.accept()
-            if len(self.clients) >= 2:
+            if len(self.player) >= 2:
                 conn.send(b'SERVER_FULL') # This is going to be the key string to stop the client
                 continue
+            
+            # Catch de nickname
+            print(f'{addr} se conectou no jogo!')
+            conn.send(b'Digite seu nick: ')
+            name = conn.recv(1024).decode()
 
-            print(f'{addr} se conectou no chat!')
-            self.clients.append(conn)
+            # Recebendo informações do jogador
+            player.addr = addr
+            player.conn = conn
+            player.name = name
 
-            conn.send(f"Olá! {addr}, obrigado por se conectar ao RPS_GAME!".encode())
+            # Adicionando jogador a lista de jogadores
+            self.players.append(player)
 
-            thread = threading.Thread(target=self.handle_player, args=(conn, addr))
+            # Dando boas vindas
+            conn.send(f"Olá! {player.name}, obrigado por se conectar ao RPS_GAME!".encode())
+
+            thread = threading.Thread(target=self.handle_player, args=(player))
             thread.start()
 
-    def handle_player(self, conn: socket.socket, addr):
-        print(f'Thread para novo cliente iniciada. Cliente {addr}')
+    def handle_player(self, player: Player):
+        while True:
+            if len(self.players == 1):
+                continue
+            else:
+                player.send_to_player("Jogo pronto!")
+                break
 
-# Função que descobre o IPV4 da maquina e printa na dela
+        player.send_to_player(self.game.get_menu())
+        
+        
+            
+
+
+
+# Função que descobre o IPV4
 def get_local_ipv4():
     try:
         # Cria um socket UDP "falso" apenas para descobrir o IP real
